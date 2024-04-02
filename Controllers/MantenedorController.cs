16ede0310d362085e5cefeb3c50cd1227b4f3a48
gaspar <gaspar@pizzati.com>
2024-04-02 12:59:33 -0600

    Terminado

--- Controllers/MantenedorController.cs
+++ Controllers/MantenedorController.cs
@@ -24,8 +24,55 @@ namespace puntoNet.Controllers
         [HttpPost]
         public IActionResult Guardar(ContactoModel oContacto)
         {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
 
             var respuesta = _contactoDatos.Guardar(oContacto);
+
+            if (respuesta)
+                return RedirectToAction("Listar");
+            return View();
+        }
+
+        public IActionResult Editar(int idContacto)
+        {
+
+            var oContacto = _contactoDatos.obtener(idContacto);
+
+            return View(oContacto);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(ContactoModel oContacto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+
+            var respuesta = _contactoDatos.Editar(oContacto);
+
+            if (respuesta)
+                return RedirectToAction("Listar");
+            return View();
+        }
+
+        public IActionResult Eliminar(int idContacto)
+        {
+
+            var oContacto = _contactoDatos.obtener(idContacto);
+
+            return View(oContacto);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(ContactoModel oContacto)
+        {
+            var respuesta = _contactoDatos.Borrar(oContacto.IdContacto);
 
             if (respuesta)
                 return RedirectToAction("Listar");
--- Models/ContactoModel.cs
+++ Models/ContactoModel.cs
@@ -1,13 +1,18 @@
-﻿namespace puntoNet.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace puntoNet.Models
 {
     public class ContactoModel
     {
         public int IdContacto { get; set; }
 
+        [Required]
         public string? Nombre { get; set; }
 
+        [Required]
         public string? Telefono { get; set; }
 
+        [Required]
         public string? Correo { get; set; }
     }
 }
